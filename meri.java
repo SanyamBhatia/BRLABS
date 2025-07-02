aabd012bc013b4479956670fc530fe64731a628b
Sanyam Bhatia <Sayambhatia47@gmail.com>
2025-07-02 12:58:07 +0530

    first commit

---
+++ meri.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class meri
+{
+
+public static void main(String[] args)
+{
+	System.out.println("Prabhat");
+	Scanner sc = new Scanner(System.in);
+	String name = sc.nextLine();
+	int roll = sc.nextInt();
+	System.out.println(name);
+	System.out.println(roll);
+	
+
+}
+}
