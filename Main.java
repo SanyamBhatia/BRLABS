3311f370510eb9f9d71d3a840f1d9557893c9017
Sanyam Bhatia <Sayambhatia47@gmail.com>
2025-07-04 12:21:50 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int num = sc.nextInt();
+        if(num%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
