fee36a20179bcdca7e1dfd0fcb1ddfb299aaff74
Sanyam Bhatia <Sayambhatia47@gmail.com>
2025-07-16 11:40:58 +0530

    first

--- Main.java
+++ Main.java
@@ -1,15 +1,34 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-import java.util.*;
-class Main {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int num = sc.nextInt();
-        if(num%2==0){
-            System.out.println("Even");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your food choice:");
+        System.out.println("1. Burger");
+        System.out.println("2. Maggi");
+        System.out.println("3. Pasta");
+        System.out.println("4. Chowmein");
+
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("You selected Burger.");
+                break;
+            case 2:
+                System.out.println("You selected Maggi.");
+                break;
+            case 3:
+                System.out.println("You selected Pasta.");
+                break;
+            case 4:
+                System.out.println("You selected Chowmein.");
+                break;
+            default:
+                System.out.println("Invalid choice. Please select from 1 to 4.");
         }
-        else{
-            System.out.println("Odd");
-        }
+
+        sc.close();
     }
 }
---
+++ Tableof2.java
@@ -0,0 +1,20 @@
+public class Tableof2 {
+    public static void main(String[] args) {
+        // for (int i = 1; i <= 10; i++) {
+        //     System.out.println("2 x " + i + " = " + (2 * i));
+        // }
+        // with while loop
+        // int i = 1;
+        // while (i <= 10) {
+        //     System.out.println("2 x " + i + " = " + (2 * i));
+        //     i++;
+        // }
+        // with do-while loop
+        int i = 1;
+        do {
+            System.out.println("2 x " + i + " = " + (2 * i));
+            i++;
+        } while (i <= 10);
+
+    }
+}
